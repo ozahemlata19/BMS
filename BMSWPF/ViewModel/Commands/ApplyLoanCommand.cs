6de5a45b51f404a01ce8f712d944061e79bdac34
VM <User@DOT-111416354-0>
2022-11-29 09:57:17 +0530

    Done

---
+++ BMSWPF/ViewModel/Commands/ApplyLoanCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BMSWPF.ViewModel.Commands
+{
+    class ApplyLoanCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public UserInterfaceVM UserInterfaceVM { get; set; }
+
+        public ApplyLoanCommand(UserInterfaceVM loan)
+        {
+            UserInterfaceVM = loan;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        
+        public void Execute(object parameter)
+        {
+            UserInterfaceVM.ExecuteLoan();
+        }
+    }
+}
---
+++ BMSWPF/ViewModel/UserInterfaceVM.cs
@@ -0,0 +1,36 @@
+﻿using BMSWPF.View;
+using BMSWPF.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BMSWPF.ViewModel
+{
+    class UserInterfaceVM : INotifyPropertyChanged
+    {
+        public ApplyLoanCommand ApplyLoanCommand { get; set; }
+        //public UpdateUserCommand UpdateUserCommand { get; set; }
+
+        public UserInterfaceVM()
+        {
+            ApplyLoanCommand = new ApplyLoanCommand(this);
+            //UpdateUserCommand = new UpdateUserCommand(this);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void ExecuteLoan()
+        {
+            ApplyLoanWindow loan = new ApplyLoanWindow();
+            loan.ShowDialog();
+        }
+    }
+}
